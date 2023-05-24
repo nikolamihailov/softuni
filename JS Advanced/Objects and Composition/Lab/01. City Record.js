58f3073177bd4394ea466645b0291a875eb9de6e
nikola mihailov <nikolanm06@abv.bg>
2023-05-24 23:58:08 +0300

    init

---
+++ JS Advanced/Objects and Composition/Lab/01. City Record.js
@@ -0,0 +1,8 @@
+function cityRecord(cityName, cityPopulation, cityTreasury) {
+    const cityRecord = {
+        name: cityName,
+        population: cityPopulation,
+        treasury: cityTreasury
+    };
+    return cityRecord;
+}
