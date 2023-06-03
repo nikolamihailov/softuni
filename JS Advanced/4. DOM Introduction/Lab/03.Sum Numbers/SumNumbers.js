cba114906e491a43e0e91f363f1086efeeccbf1a
nikola mihailov <nikolanm06@abv.bg>
2023-06-03 21:50:04 +0300

    added task 3

---
+++ JS Advanced/4. DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const sum = document.getElementById("sum");
+    sum.value = Number(num1) + Number(num2);
+}
