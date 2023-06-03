d10cf896cd01a8042444644b44ce94004b72e987
nikola mihailov <nikolanm06@abv.bg>
2023-06-04 00:54:27 +0300

    added task 2

--- JS Advanced/4. DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ JS Advanced/4. DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO...
+  const text = document.getElementById("text").value.toLowerCase();
+  const namingConvention = document.getElementById("naming-convention").value;
+  const result = document.getElementById("result");
+  const words = text.split(" ");
+  if (namingConvention === "Camel Case") {
+    const changedWords = words.map(w => w = w[0].toUpperCase() + w.slice(1));
+    changedWords[0] = changedWords[0][0].toLowerCase() + changedWords[0].slice(1);
+    result.textContent = changedWords.join("");
+  } else if (namingConvention === "Pascal Case") {
+    result.textContent = words.map(w => w = w[0].toUpperCase() + w.slice(1)).join("");
+  } else {
+    result.textContent = "Error!";
+  }
 }
