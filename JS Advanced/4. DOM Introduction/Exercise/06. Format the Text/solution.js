b59306b17cc8bb1046c11082e303b308f2e457b2
nikola mihailov <nikolanm06@abv.bg>
2023-06-04 13:50:32 +0300

    fixed >= problem

--- JS Advanced/4. DOM Introduction/Exercise/06. Format the Text/solution.js
+++ JS Advanced/4. DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,7 +1,7 @@
 function solve() {
   const textarea = document.getElementById("input");
   const output = document.getElementById("output");
-  const sentences = textarea.value.split(".").filter(s => s.length > 1);
+  const sentences = textarea.value.split(".").filter(s => s.length >= 1);
   while (sentences.length > 0) {
     const p = document.createElement("p");
     p.textContent = sentences.splice(0, 3).join(".") + ".";
