c2a6a0b4e1d51c02205add4e8b72f588f7527d8e
nikola mihailov <nikolanm06@abv.bg>
2023-06-04 14:05:37 +0300

    exercise finished

--- JS Advanced/4. DOM Introduction/Exercise/09. Number Convertor/solution.js
+++ JS Advanced/4. DOM Introduction/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,15 @@
 function solve() {
-
-    //TODO...
+    const result = document.getElementById("result");
+    const toValue = document.getElementById("selectMenuTo");
+    toValue.innerHTML = ` <option selected value="binary">Binary</option>
+                          <option value="hexadecimal">Hexadecimal</option>`;
+    const button = document.querySelector("button");
+    button.addEventListener("click", () => {
+        const input = Number(document.getElementById("input").value);
+        if (toValue.value === "binary") {
+            result.value = input.toString(2);
+        } else {
+            result.value = input.toString(16).toUpperCase();
+        }
+    });
 }
