b1af6a4948df0ac06af3bf0b28fc419a3e2e376a
nikola mihailov <nikolanm06@abv.bg>
2023-06-07 22:30:00 +0300

    added task 1

---
+++ JS Advanced/7. Unit Testing and Error Handling/Lab/01. Sub Sum.js
@@ -0,0 +1,16 @@
+function subSum(arr, startIdx, endIdx) {
+    if (startIdx < 0) startIdx = 0;
+    if (endIdx >= arr.length) endIdx = arr.length - 1;
+    if (!Array.isArray(arr)) return NaN;
+
+    let sum = 0;
+    for (let i = startIdx; i <= endIdx; i++) {
+        sum += Number(arr[i]);
+    }
+    return sum;
+}
+console.log(subSum([10, 20, 30, 40, 50, 60], 3, 5));
+console.log(subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1));
+console.log(subSum([10, 'twenty', 30, 40], 0, 2));
+console.log(subSum([], 1, 2));
+console.log(subSum('text', 0, 2));
