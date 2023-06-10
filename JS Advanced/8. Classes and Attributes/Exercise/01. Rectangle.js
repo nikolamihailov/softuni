6460bf8acde0f85009a8d5be86c6180b559da945
nikola mihailov <nikolanm06@abv.bg>
2023-06-10 16:24:27 +0300

    added task 1

---
+++ JS Advanced/8. Classes and Attributes/Exercise/01. Rectangle.js
@@ -0,0 +1,15 @@
+class Rectangle {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    calcArea() {
+        return this.width * this.height;
+    }
+}
+const rect = new Rectangle(4, 5, 'Red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
