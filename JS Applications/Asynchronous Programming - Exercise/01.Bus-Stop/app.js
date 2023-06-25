da0f75da3041e619569830017fa7da74d6fbe74a
nikola mihailov <nikolanm06@abv.bg>
2023-06-25 21:38:06 +0300

    task one finished

--- JS Applications/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ JS Applications/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,30 @@
 function getInfo() {
-    console.log("TODO...");
+
+    async function fetchData() {
+        const stopId = document.getElementById("stopId");
+        const stopName = document.getElementById("stopName");
+        const buses = document.getElementById("buses");
+
+        try {
+            const response = await fetch('http://localhost:3030/jsonstore/bus/businfo/' + stopId.value);
+            if (!response.ok) {
+                throw new Error("Error");
+            }
+            const data = await response.json();
+            stopName.textContent = "";
+            buses.innerHTML = "";
+            stopName.textContent = data.name;
+            const stops = Object.entries(data.buses);
+            stops.forEach(([bus, minutes]) => {
+                const li = document.createElement("li");
+                li.textContent = `Bus ${bus} arrives in ${minutes} minutes`;
+                buses.appendChild(li);
+            });
+        } catch (error) {
+            stopName.textContent = "Error";
+            buses.innerHTML = "";
+            return;
+        }
+    }
+    fetchData();
 }
