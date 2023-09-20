05eb89c3f6fcf6c14db39441d325fc273b6225f1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-09-20 13:51:01 +0300

    moved comments to right place

--- JS Back-End/Cubicle/src/index.js
+++ JS Back-End/Cubicle/src/index.js
@@ -2,17 +2,14 @@ const PORT = 5050;
 const express = require("express");
 
 const expressConfig = require("./config/expressConfig");
+// or  require("./config/expressConfig")(app)
 const handlebarsConfig = require("./config/handlebarsConfig");
 const routes = require("../src/routes");
 const app = express();
 
 expressConfig(app);
 handlebarsConfig(app);
-app.use(routes);
-// or  require("./config/expressConfig")(app)
-
 // Routes
-
-
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT} ...`));
