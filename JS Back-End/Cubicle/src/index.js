f62d112e274c3c8ca0ee968aaa998d42e0b6cf28
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-09-23 13:55:15 +0300

    connected the application to the db

--- JS Back-End/Cubicle/src/index.js
+++ JS Back-End/Cubicle/src/index.js
@@ -1,11 +1,16 @@
-const PORT = 5050;
+const { PORT } = require("./constants");
 const express = require("express");
 
 const expressConfig = require("./config/expressConfig");
 // or  require("./config/expressConfig")(app)
 const handlebarsConfig = require("./config/handlebarsConfig");
+const dbConnect = require("./config/dbConfig");
 const routes = require("../src/routes");
 const app = express();
+
+dbConnect()
+    .then(() => console.log("DB connected!"))
+    .catch((err) => console.log(`Db error ${err}`));
 
 expressConfig(app);
 handlebarsConfig(app);
