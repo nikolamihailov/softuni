a23612a474608c920f4b5c3a0da3468837ae4170
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-09-28 15:20:28 +0300

    added base User model

--- JS Back-End/Cubicle/src/controllers/userController.js
+++ JS Back-End/Cubicle/src/controllers/userController.js
@@ -8,6 +8,12 @@ router.get("/register", (req, res) => {
     res.render("user/register");
 });
 
+router.post("/register", (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+    console.log(username, password, repeatPassword);
+    res.redirect("/users/login");
+});
+
 router.get("/logout", (req, res) => {
     res.render("index");
 });
