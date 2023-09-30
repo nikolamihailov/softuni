c2d8b00d3f73861598b164b8f50c2075483c0bfe
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-09-30 14:42:25 +0300

    moved the secret for jwt

--- JS Back-End/Cubicle/src/constants.js
+++ JS Back-End/Cubicle/src/constants.js
@@ -1,7 +1,9 @@
 const PORT = 5050;
 const CONNECTION_STR = "mongodb://127.0.0.1:27017/cubicle-db";
+const SECRET = "alabala";
 
 module.exports = {
     PORT,
-    CONNECTION_STR
+    CONNECTION_STR,
+    SECRET
 };
