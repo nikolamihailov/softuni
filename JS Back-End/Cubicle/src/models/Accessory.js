1c2497a0edc9ecc9ae9f4a04785af23d3c9a9056
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-01 23:55:10 +0300

    fixed accessory schema validation

--- JS Back-End/Cubicle/src/models/Accessory.js
+++ JS Back-End/Cubicle/src/models/Accessory.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
 const accessorySchema = mongoose.Schema({
-    name: String,
-    description: String,
-    imageUrl: String
+    name: {
+        type: String,
+        required: [true, "Accessory name is required!"],
+        minLength: [5, "Accessory name must be at least 5 chars!"],
+        match: [/^[A-Za-z0-9 ]$/, "Accessory name must be alphanumeric!"]
+    },
+    description: {
+        type: String,
+        minLength: [20, "Accessory description must be at least 20 chars!"],
+        match: [/^[A-Za-z0-9 ]$/, "Accessory description must be alphanumeric!"]
+    },
+    imageUrl: {
+        type: String,
+        required: [true, "Accessory image is required!"],
+        match: [/^https?:\/\/.+/, "Provide valid accessory image link!"]
+    }
 });
 
 const Accessory = mongoose.model("Accessory", accessorySchema);
