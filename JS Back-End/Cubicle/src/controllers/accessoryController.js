d10ecc2edc33d16e85de244f963f7711d0411b45
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-02 00:20:14 +0300

    fixed accessory controller validation

--- JS Back-End/Cubicle/src/controllers/accessoryController.js
+++ JS Back-End/Cubicle/src/controllers/accessoryController.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const accessoryService = require("../services/accessoryService");
+const { extractErrorMessages } = require("../utils/errorHelper");
 
 router.get("/create", (req, res) => {
     res.render("accessory/create");
@@ -7,9 +8,13 @@ router.get("/create", (req, res) => {
 
 router.post("/create", async (req, res) => {
     const { name, description, imageUrl } = req.body;
-    console.log(name, description, imageUrl);
-    await accessoryService.create({ name, description, imageUrl });
-    res.redirect("/");
+    try {
+        await accessoryService.create({ name, description, imageUrl });
+        res.redirect("/");
+    } catch (error) {
+        const errors = extractErrorMessages(error);
+        res.render("accessory/create", { errors });
+    }
 });
 
 
