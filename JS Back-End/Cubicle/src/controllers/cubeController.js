56d42e467b8d2c49a2c38e041293218d95a101e9
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-02 01:05:40 +0300

    changed cube controller actions error handling

--- JS Back-End/Cubicle/src/controllers/cubeController.js
+++ JS Back-End/Cubicle/src/controllers/cubeController.js
@@ -11,12 +11,15 @@ router.get("/create", isAuth, (req, res) => {
 });
 
 router.get("/:cubeId/details", async (req, res) => {
-    const cube = await cubeService.getCubeById(req.params.cubeId);
-    const accessories = cube.accessories;
-    if (!cube) return res.redirect("/404-page-not-found");
-    let isOwner = req.user?._id === cube.owner?.toString();
-    if (req.user?._id === undefined && cube.owner?.toString() === undefined) isOwner = false;
-    res.render("cube/details", { cube, accessories, isOwner });
+    try {
+        const cube = await cubeService.getCubeById(req.params.cubeId);
+        const accessories = cube.accessories;
+        let isOwner = req.user?._id === cube.owner?.toString();
+        if (req.user?._id === undefined && cube.owner?.toString() === undefined) isOwner = false;
+        res.render("cube/details", { cube, accessories, isOwner });
+    } catch (error) {
+        res.redirect("/404-page-not-found");
+    }
 });
 
 router.post("/create", isAuth, async (req, res) => {
@@ -31,10 +34,14 @@ router.post("/create", isAuth, async (req, res) => {
 });
 
 router.get("/:cubeId/attach-accessory", isAuth, async (req, res) => {
-    const cube = await cubeService.getCubeById(req.params.cubeId);
-    const accessories = await accessoryService.getAvailableAccessories(cube.accessories);
-    const hasAccessories = accessories.length > 0;
-    res.render("accessory/attach", { cube, accessories, hasAccessories });
+    try {
+        const cube = await cubeService.getCubeById(req.params.cubeId);
+        const accessories = await accessoryService.getAvailableAccessories(cube.accessories);
+        const hasAccessories = accessories.length > 0;
+        res.render("accessory/attach", { cube, accessories, hasAccessories });
+    } catch (error) {
+        res.redirect("/404-page-not-found");
+    }
 });
 
 router.post("/:cubeId/attach-accessory", isAuth, async (req, res) => {
@@ -50,9 +57,13 @@ router.post("/:cubeId/attach-accessory", isAuth, async (req, res) => {
 });
 
 router.get("/:cubeId/delete", isAuth, async (req, res) => {
-    const cube = await cubeService.getCubeById(req.params.cubeId);
-    const options = getDifficultyOptionsViewData(cube.difficultyLevel);
-    res.render("cube/delete", { cube, options });
+    try {
+        const cube = await cubeService.getCubeById(req.params.cubeId);
+        const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+        res.render("cube/delete", { cube, options });
+    } catch (error) {
+        res.redirect("/404-page-not-found");
+    }
 });
 
 router.post("/:cubeId/delete", isAuth, isAuth, async (req, res) => {
@@ -67,6 +78,7 @@ router.post("/:cubeId/delete", isAuth, isAuth, async (req, res) => {
 
 router.get("/:cubeId/edit", isAuth, async (req, res) => {
     const cube = await cubeService.getCubeById(req.params.cubeId);
+    if (!cube) return res.redirect("/404-page-not-found");
     const options = getDifficultyOptionsViewData(cube.difficultyLevel);
     res.render("cube/edit", { cube, options });
 });
@@ -74,14 +86,20 @@ router.get("/:cubeId/edit", isAuth, async (req, res) => {
 router.post("/:cubeId/edit", isAuth, async (req, res) => {
     const cubeData = req.body;
     try {
-        if (!cubeData.name || cubeData.name.trim().length < 5) throw new Error("Cube name is required (5 caharacters) and must be alphanumeric!");
-        if (!cubeData.description || cubeData.description.trim().length < 20) throw new Error("Cube description is required (20 characters) and must be alphanumeric!");
-        if (!cubeData.imageUrl || !`/^https?:\/\/.+/`.test(cubeData.imageUrl)) throw new Error("Cube image is required and must be valid link!");
+        const errors = [];
+        if (!cubeData.name || cubeData.name.trim().length < 5) errors.push("Cube name is required (5 characters) and must be alphanumeric!");
+        if (!cubeData.description || cubeData.description.trim().length < 20) errors.push("Cube description is required (20 characters) and must be alphanumeric!");
+        if (!cubeData.imageUrl || !/^https?:\/\/.+/.test(cubeData.imageUrl)) errors.push("Cube image is required and must be a valid link!");
+
+        if (errors.length > 0) {
+            throw new Error(errors.join(" "));
+        }
         await cubeService.updateCube(req.params.cubeId, cubeData, { runValidators: true });
         res.redirect(`/cubes/${req.params.cubeId}/details`);
     } catch (error) {
-        const errors = extractErrorMessages(error);
-        res.render("cube/edit", { errors });
+        const cube = await cubeService.getCubeById(req.params.cubeId);
+        const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+        res.render("cube/edit", { cube, options, errors: error.message.split("! ") });
     }
 });
 
