20e69128fe5adf45a50bb2565d9484d1bc1690a6
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-02 00:18:04 +0300

    fixed cube controller validations

--- JS Back-End/Cubicle/src/controllers/cubeController.js
+++ JS Back-End/Cubicle/src/controllers/cubeController.js
@@ -3,6 +3,7 @@ const cubeService = require("../services/cubeService");
 const accessoryService = require("../services/accessoryService");
 const { getDifficultyOptionsViewData } = require("../utils/viewHelpers");
 const { isAuth } = require("../middlewares/authMiddleware");
+const { extractErrorMessages } = require("../utils/errorHelper");
 // path /cubes/create
 router.get("/create", isAuth, (req, res) => {
     // console.log(cubeService.getAllCubes());
@@ -20,8 +21,13 @@ router.get("/:cubeId/details", async (req, res) => {
 
 router.post("/create", isAuth, async (req, res) => {
     const { name, description, imageUrl, difficultyLevel } = req.body;
-    await cubeService.create({ name, description, imageUrl, difficultyLevel: +difficultyLevel, owner: req.user._id });
-    res.redirect("/");
+    try {
+        await cubeService.create({ name, description, imageUrl, difficultyLevel: +difficultyLevel, owner: req.user._id });
+        res.redirect("/");
+    } catch (error) {
+        const errors = extractErrorMessages(error);
+        res.render("cube/create", { errors });
+    }
 });
 
 router.get("/:cubeId/attach-accessory", isAuth, async (req, res) => {
@@ -34,8 +40,13 @@ router.get("/:cubeId/attach-accessory", isAuth, async (req, res) => {
 router.post("/:cubeId/attach-accessory", isAuth, async (req, res) => {
     const { accessory: accessoryId } = req.body;
     const cubeId = req.params.cubeId;
-    await cubeService.attachAccessory(cubeId, accessoryId);
-    res.redirect(`/cubes/${cubeId}/details`);
+    try {
+        await cubeService.attachAccessory(cubeId, accessoryId);
+        res.redirect(`/cubes/${cubeId}/details`);
+    } catch (error) {
+        const errors = extractErrorMessages(error);
+        res.render("accessory/attach", { errors });
+    }
 });
 
 router.get("/:cubeId/delete", isAuth, async (req, res) => {
@@ -45,8 +56,13 @@ router.get("/:cubeId/delete", isAuth, async (req, res) => {
 });
 
 router.post("/:cubeId/delete", isAuth, isAuth, async (req, res) => {
-    await cubeService.deleteCube(req.params.cubeId);
-    res.redirect("/");
+    try {
+        await cubeService.deleteCube(req.params.cubeId);
+        res.redirect("/");
+    } catch (error) {
+        const errors = extractErrorMessages(error);
+        res.render("cube/delete", { errors });
+    }
 });
 
 router.get("/:cubeId/edit", isAuth, async (req, res) => {
@@ -57,8 +73,16 @@ router.get("/:cubeId/edit", isAuth, async (req, res) => {
 
 router.post("/:cubeId/edit", isAuth, async (req, res) => {
     const cubeData = req.body;
-    await cubeService.updateCube(req.params.cubeId, cubeData);
-    res.redirect(`/cubes/${req.params.cubeId}/details`);
+    try {
+        if (!cubeData.name || cubeData.name.trim().length < 5) throw new Error("Cube name is required (5 caharacters) and must be alphanumeric!");
+        if (!cubeData.description || cubeData.description.trim().length < 20) throw new Error("Cube description is required (20 characters) and must be alphanumeric!");
+        if (!cubeData.imageUrl || !`/^https?:\/\/.+/`.test(cubeData.imageUrl)) throw new Error("Cube image is required and must be valid link!");
+        await cubeService.updateCube(req.params.cubeId, cubeData, { runValidators: true });
+        res.redirect(`/cubes/${req.params.cubeId}/details`);
+    } catch (error) {
+        const errors = extractErrorMessages(error);
+        res.render("cube/edit", { errors });
+    }
 });
 
 module.exports = router;
