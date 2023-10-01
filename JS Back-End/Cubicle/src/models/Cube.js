16e1a4c11bb7bd39a35835c5309dd6eb9083643c
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-01 23:53:25 +0300

    fixed cube model validation

--- JS Back-End/Cubicle/src/models/Cube.js
+++ JS Back-End/Cubicle/src/models/Cube.js
@@ -1,10 +1,26 @@
 const mongoose = require("mongoose");
 
 const cubeSchema = mongoose.Schema({
-    name: String,
-    description: String,
-    imageUrl: String,
-    difficultyLevel: Number,
+    name: {
+        type: String,
+        required: [true, "Cube name is required!"],
+        minLength: [5, "Cube name must be at least 5 chars!"],
+        match: [/^[A-Za-z0-9 ]$/, "Cube name must be alphanumeric!"]
+    },
+    description: {
+        type: String,
+        minLength: [20, "Cube description must be at least 20 chars!"],
+        match: [/^[A-Za-z0-9 ]$/, "Cube description must be alphanumeric!"]
+    },
+    imageUrl: {
+        type: String,
+        required: [true, "Cube image is required!"],
+        match: [/^https?:\/\/.+/, "Provide valid cube image link!"]
+    },
+    difficultyLevel: {
+        type: Number,
+        required: [true, "Cube difficulty is required!"],
+    },
     accessories: [{
         type: mongoose.Types.ObjectId,
         ref: "Accessory"
