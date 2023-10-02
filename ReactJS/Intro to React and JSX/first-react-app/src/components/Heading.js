37e9dcf7f5c6815232516fcdd3abc4dfe6627fe1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-02 21:12:52 +0300

    created components

---
+++ ReactJS/Intro to React and JSX/first-react-app/src/components/Heading.js
@@ -0,0 +1,5 @@
+const Heading = () => {
+    return <h1>Professional React App</h1>;
+};
+
+export default Heading;
---
+++ ReactJS/Intro to React and JSX/first-react-app/src/components/Logo.js
@@ -0,0 +1,7 @@
+import logo from '../logo.svg';
+
+function Logo() {
+    return <img src={logo} className="App-logo" alt="logo" />;
+}
+
+export default Logo;
