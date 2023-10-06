d1fb9f9091bcc709068a14b91e154f7e2085a0de
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-06 15:10:46 +0300

    added user to locals for welcome user msg

--- JS Back-End/Cubicle/src/middlewares/authMiddleware.js
+++ JS Back-End/Cubicle/src/middlewares/authMiddleware.js
@@ -9,6 +9,7 @@ exports.auth = async (req, res, next) => {
         try {
             const user = await jwt.verify(token, SECRET);
             req.user = user;
+            res.locals.user = user;
             res.locals.isAuthenticated = true;
             next();
         } catch (error) {
