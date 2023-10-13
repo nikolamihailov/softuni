823afe69b494971f56c9188eeeeb96f288010e64
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-13 15:14:56 +0300

    app base done

--- JS Back-end Exams/Exam scaffold/src/index.js
+++ JS Back-end Exams/Exam scaffold/src/index.js
@@ -1,1 +1,14 @@
-console.log("started");
+const express = require("express");
+
+const app = express();
+const expressConfig = require("./config/expressConfig");
+const handlebarsConfig = require("./config/handlebarsConfig");
+
+expressConfig(app);
+handlebarsConfig(app);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(3000, () => console.log("Server is listneing on port 3000"));
