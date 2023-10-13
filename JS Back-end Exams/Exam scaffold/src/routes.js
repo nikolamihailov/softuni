0c207034b9fb19823c56b2aaa35140840c157f18
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-13 16:01:03 +0300

    homeController added

--- JS Back-end Exams/Exam scaffold/src/routes.js
+++ JS Back-end Exams/Exam scaffold/src/routes.js
@@ -1,15 +1,13 @@
 const router = require("express").Router();
 
+const homeController = require("./controllers/homeController");
 const userContoller = require("./controllers/userConroller");
 
+router.use(homeController);
 router.use("/users", userContoller);
 
-router.get("/", (req, res) => {
-    res.render("home");
-});
-
 router.get("*", (req, res) => {
-    res.render("404");
+    res.redirect("/error-404-page");
 });
 
 module.exports = router;
