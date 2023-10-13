a1667c875ad7ba3c41029b4049645eed73ffa311
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-13 21:34:31 +0300

    dbConfig added

--- JS Back-end Exams/Exam scaffold/src/constants.js
+++ JS Back-end Exams/Exam scaffold/src/constants.js
@@ -1,5 +1,9 @@
-const PORT = 3000;
+const PORT = 5050;
+const CONN_STR = "mongodb://127.0.0.1:27017/";
+const DB_NAME = "demo-db";
 
 module.exports = {
-    PORT
+    PORT,
+    CONN_STR,
+    DB_NAME
 };
--- JS Back-end Exams/Exam scaffold/src/index.js
+++ JS Back-end Exams/Exam scaffold/src/index.js
@@ -1,10 +1,14 @@
-const express = require("express");
+const app = require("express")();
 const { PORT } = require("./constants");
 
-const app = express();
 const expressConfig = require("./config/expressConfig");
 const handlebarsConfig = require("./config/handlebarsConfig");
+const connectToDb = require("./config/dbConfig");
 const routes = require("./routes");
+
+connectToDb()
+    .then(() => console.log("DB connected!"))
+    .catch((err) => console.log(`Db error ${err}`));
 
 expressConfig(app);
 handlebarsConfig(app);
