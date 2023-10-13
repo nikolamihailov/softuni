298fa42fefcb19de54d2adf535008111608580b2
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-14 01:11:17 +0300

    added logout

--- JS Back-end Exams/Exam scaffold/src/controllers/userConroller.js
+++ JS Back-end Exams/Exam scaffold/src/controllers/userConroller.js
@@ -35,6 +35,11 @@ router.post("/login", async (req, res) => {
     }
 });
 
+router.get("/logout", (req, res) => {
+    res.clearCookie("auth");
+    res.redirect("/");
+});
+
 
 module.exports = router;
 
