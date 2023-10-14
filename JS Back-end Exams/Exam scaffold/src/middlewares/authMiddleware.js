1a1be19a294cca1d6e8ac1d767a18c685cbf5bb5
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-14 20:01:50 +0300

    mdw fixed

--- JS Back-end Exams/Exam scaffold/src/middlewares/authMiddleware.js
+++ JS Back-end Exams/Exam scaffold/src/middlewares/authMiddleware.js
@@ -1,18 +1,18 @@
-const bcrypt = require("bcrypt");
 const { SECRET } = require("../constants");
+const jwt = require("../lib/jwt");
 
 exports.isAuth = async (req, res, next) => {
     const token = req.cookies["auth"];
     if (token) {
         try {
-            const decodedToken = await bcrypt.compare(token, SECRET);
+            const decodedToken = await jwt.verify(token, SECRET);
             req.user = decodedToken;
             res.locals.user = decodedToken;
             res.locals.isAuthenticated = true;
             next();
         } catch (error) {
             res.clearCookie("auth");
-            res.redirect("/users/login");
+            return res.redirect("/users/login");
         }
     } else {
         next();
@@ -20,6 +20,6 @@ exports.isAuth = async (req, res, next) => {
 };
 
 exports.auth = (req, res, next) => {
-    if (!req.user) res.redirect("/users/login");
+    if (!req.user) return res.redirect("/users/login");
     next();
 };
