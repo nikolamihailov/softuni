b21199c444abb4feceeb1985d2298492e15c708d
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-14 22:53:14 +0300

    fixed port and added img to public

--- JS Back-end Exams/Wizard-Creatures-Wiki/src/constants.js
+++ JS Back-end Exams/Wizard-Creatures-Wiki/src/constants.js
@@ -1,4 +1,4 @@
-const PORT = 3200;
+const PORT = 3000;
 const CONN_STR = "mongodb://127.0.0.1:27017/";
 const DB_NAME = "wizard-creatures-db";
 const SECRET = "djcbd44dhj45cbdhsjbsjbsds56dc4dc4dcdc";
