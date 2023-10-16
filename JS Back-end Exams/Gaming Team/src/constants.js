5dddcfee0437933d16f146e8a32ce1cdbd1ac344
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-16 21:59:51 +0300

    initial structure done

--- JS Back-end Exams/Gaming Team/src/constants.js
+++ JS Back-end Exams/Gaming Team/src/constants.js
@@ -1,6 +1,6 @@
 const PORT = 3000;
 const CONN_STR = "mongodb://127.0.0.1:27017/";
-const DB_NAME = "demo-db";
+const DB_NAME = "gaming-team-db";
 const SECRET = "djcbd44dhj45cbdhsjbsjbsds56dc4dc4dcdc";
 
 module.exports = {
