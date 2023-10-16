418429a9ae26bf996214e1c47cdbd4368f3cc1f8
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-16 21:49:57 +0300

    exam started

---
+++ JS Back-end Exams/Gaming Team/src/lib/jwt.js
@@ -0,0 +1,10 @@
+const { promisify } = require("util");
+const jsonwebtoken = require("jsonwebtoken");
+
+const jwt = {
+    sign: promisify(jsonwebtoken.sign),
+    verify: promisify(jsonwebtoken.verify),
+};
+
+module.exports = jwt;
+
