17ad9cc156e7ac026f22a79bfd53b3c92a59a443
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-21 13:45:07 +0300

    initial structure done

---
+++ JS Back-end Exams/Regular Exam/src/services/userService.js
@@ -0,0 +1,26 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt");
+const { SECRET } = require("../constants");
+
+exports.register = async (userData) => {
+    const { firstName, lastName, email } = userData;
+    const user = await User.create(userData);
+    const payload = { _id: user._id, firstName, lastName, email };
+    const token = await jwt.sign(payload, SECRET, { expiresIn: "1d" });
+    return token;
+};
+
+exports.login = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) throw new Error("Invalid email or password");
+
+    const isPassCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPassCorrect) throw new Error("Invalid email or password");
+
+    const payload = { _id: user._id, email, firstName: user.firstName, lastName: user.lastName };
+    const token = await jwt.sign(payload, SECRET, { expiresIn: "1d" });
+    return token;
+};
