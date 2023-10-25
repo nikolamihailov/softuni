4322646e28f873aef87b1dc64dd038e4901bfa7c
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-25 18:58:30 +0300

    regular exam finished 6.00

--- JS Back-end Exams/Regular Exam/src/utils/errorHelper.js
+++ JS Back-end Exams/Regular Exam/src/utils/errorHelper.js
@@ -2,9 +2,6 @@ const { MongooseError } = require("mongoose");
 
 exports.extractErrors = (error) => {
     const isMongooseError = error instanceof MongooseError;
-    if (isMongooseError) {
-        return Object.values(error.errors).map(e => e.message);
-    } else {
-        return [error.message];
-    }
+    if (isMongooseError) return Object.values(error.errors).map(e => e.message);
+    else return [error.message];
 };
