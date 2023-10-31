a132e5ea30225805915fb237996688df77135234
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-31 23:59:19 +0200

    login added

--- JS Back-End/05. Workshop REST API/server/src/controllers/userController.js
+++ JS Back-End/05. Workshop REST API/server/src/controllers/userController.js
@@ -17,4 +17,20 @@ router.post("/register", async (req, res) => {
     }
 });
 
+router.post("/login", async (req, res) => {
+    try {
+        console.log(req.body);
+        // const { email, password, rePass } = req.body;
+        const user = await userService.register({ ...req.body });
+
+        res.json({
+            email: user.email,
+            accessToken,
+            _id: user._id
+        });
+    } catch (error) {
+
+    }
+});
+
 module.exports = router;
