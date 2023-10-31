422ab88cf7b12874fae1e72d819e7e77fadb938b
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-31 17:50:35 +0200

    added cors headers

--- JS Back-End/05. Workshop REST API/server/src/index.js
+++ JS Back-End/05. Workshop REST API/server/src/index.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("Hello there");
 });
