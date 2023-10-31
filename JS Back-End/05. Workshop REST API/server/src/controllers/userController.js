ebb6821ecf2b4bacceeb01bf4c4640da7b818c57
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-31 23:55:03 +0200

    updated user controller

--- JS Back-End/05. Workshop REST API/server/src/controllers/userController.js
+++ JS Back-End/05. Workshop REST API/server/src/controllers/userController.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
+const userService = require("../services/userService");
 
-router.post("/register", (req, res) => {
-    const { email, password } = req.body;
-    console.log(email, password);
-    res.send("ok");
+router.post("/register", async (req, res) => {
+    try {
+        console.log(req.body);
+        // const { email, password, rePass } = req.body;
+        const user = await userService.register({ ...req.body });
+
+        res.json({
+            email: user.email,
+            authToken,
+            userId: user._id
+        });
+    } catch (error) {
+
+    }
 });
 
 module.exports = router;
