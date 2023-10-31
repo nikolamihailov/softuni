e42bb0f41176f02e1d8f9bc51ee8b74a9f47f7dd
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-10-31 23:41:10 +0200

    cors mdw added and register

--- JS Back-End/05. Workshop REST API/server/src/controllers/userController.js
+++ JS Back-End/05. Workshop REST API/server/src/controllers/userController.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
 
+router.post("/register", (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
+    res.send("ok");
+});
+
 module.exports = router;
--- JS Back-End/05. Workshop REST API/server/src/index.js
+++ JS Back-End/05. Workshop REST API/server/src/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
     next();
-});
+}); */
 
 app.get("/", (req, res) => {
     res.send("Hello there");
