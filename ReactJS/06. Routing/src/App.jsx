41060bc4d7e0d045364ee697927a232991bd1cae
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-03 16:38:59 +0200

    added navigation component

--- ReactJS/06. Routing/src/App.jsx
+++ ReactJS/06. Routing/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
       <header>
+        <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ ReactJS/06. Routing/src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <li>
+        {/* wrapper of anchor tag, still <a></a> DOM element */}
+        <Link to={"/"}>Home</Link>
+      </li>
+      {/*  Not proper way - a tag refreshes the page on redirect
+     <li> 
+        <a href="/about">About</a>
+      </li> */}
+      <li>
+        <Link to={"/about"}>About</Link>
+      </li>
+    </nav>
+  );
+};
+
+export default Navigation;
