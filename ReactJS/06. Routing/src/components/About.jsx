345a653db4df856ac1609f90027df3b1fc854c03
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-03 16:16:03 +0200

    successfully added navigation

---
+++ ReactJS/06. Routing/src/components/About.jsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <h2>Welcome to About page!</h2>;
+};
+
+export default About;
---
+++ ReactJS/06. Routing/src/components/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h2>Welcome to Home page!</h2>;
+};
+
+export default Home;
