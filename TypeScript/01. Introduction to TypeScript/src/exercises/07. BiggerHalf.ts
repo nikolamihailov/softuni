04ae47f9150c6c6cae84f32e690a40a2d17908ef
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-04-02 23:39:11 +0300

    task 7 solved correctly

--- TypeScript/01. Introduction to TypeScript/src/exercises/07. BiggerHalf.ts
+++ TypeScript/01. Introduction to TypeScript/src/exercises/07. BiggerHalf.ts
@@ -1,7 +1,7 @@
 function biggerHalf(nums: number[]): number[] {
-  const half: number = Math.ceil(nums.length / 2);
+  const half: number = Math.floor(nums.length / 2);
   return nums.sort((a, b) => a - b).slice(half);
 }
 
 console.log(biggerHalf([4, 7, 2, 5]));
-console.log(biggerHalf([7, 14, 19, 19]));
+console.log(biggerHalf([3, 19, 14, 7, 2, 19, 6]));
