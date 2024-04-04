796e0d9a07b3daa77efe46ece944883a22c7a6f6
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-04-04 23:37:59 +0300

    new example added

--- TypeScript/02. Advanced Data Types and Interfaces/src/01. CalorieObject.ts
+++ TypeScript/02. Advanced Data Types and Interfaces/src/01. CalorieObject.ts
@@ -9,3 +9,4 @@ function calorieObject(food: string[]): object {
 }
 
 console.log(calorieObject(["Yoghurt", "48", "Rise", "138", "Apple", "52"]));
+console.log(calorieObject(["Potato", "93", "Skyr", "63", "Cucumber", "18", "Milk", "42"]));
