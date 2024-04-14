659c98520ef0b59abd3214007015c2ab5133c0f5
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-04-14 23:23:16 +0300

    task 4 solved

---
+++ TypeScript/02. Advanced Data Types and Interfaces/src/04. Towns.ts
@@ -0,0 +1,22 @@
+type Town = {
+  town: string;
+  latitude: string;
+  longitude: string;
+};
+
+function printTowwsn(townsInfo: string[]): Town[] {
+  const towns: Town[] = [];
+
+  townsInfo.forEach((townInfo) => {
+    const [townName, latitude, longitude] = townInfo.split(" | ");
+    const town: Town = {
+      town: townName,
+      latitude: Number(latitude).toFixed(2),
+      longitude: Number(longitude).toFixed(2),
+    };
+    towns.push(town);
+  });
+
+  return towns;
+}
+console.log(printTowwsn(["Sofia | 42.696552 | 23.32601", "Beijing | 39.913818 | 116.363625"]));
