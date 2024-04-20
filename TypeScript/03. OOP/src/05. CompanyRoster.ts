6850b294169e1328e728db936d3f06f5bde9453c
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-04-20 19:38:50 +0300

    another test for task 5 added

--- TypeScript/03. OOP/src/05. CompanyRoster.ts
+++ TypeScript/03. OOP/src/05. CompanyRoster.ts
@@ -133,11 +133,22 @@ function highestPaidDepartment(employeeCount: number, employeesInfo: string[]): 
   return finalStr;
 }
 
-const res = highestPaidDepartment(4, [
+/* const res = highestPaidDepartment(4, [
   "Peter 120.00 Dev Development peter@abv.bg 28",
   "Tina 333.33 Manager Marketing 33",
   "Sam 840.20 ProjectLeader Development sam@sam.com",
   "George 0.20 Freeloader Nowhere 18",
+]);
+
+console.log(res); */
+
+const res = highestPaidDepartment(6, [
+  "Silver 496.37 Temp Coding silver@yahoo.com",
+  "Sam 610.13 Manager Sales",
+  "John 609.99 Manager Sales john@abv.bg 44",
+  "Venci 0.02 Director BeerDrinking beer@beer.br 23",
+  "Andre 700.00 Director Coding",
+  "Popeye 13.3333 Sailor SpinachGroup popeye@pop.ey",
 ]);
 
 console.log(res);
