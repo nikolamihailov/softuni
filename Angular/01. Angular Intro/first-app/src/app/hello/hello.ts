58764c2b0b82dd31a6a41aff0e291a30ce71ae7d
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-06-29 17:21:32 +0300

    create first angular app

---
+++ Angular/01. Angular Intro/first-app/src/app/hello/hello.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.css'
+})
+export class Hello {
+
+}
