428778fe652f1e337e04a45aeec86246590d6437
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-07-15 21:35:22 +0300

    push progress for data binding, for, if and operators

--- Angular/02. Components/src/app/greeting/greeting.ts
+++ Angular/02. Components/src/app/greeting/greeting.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-greeting',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './greeting.css'
 })
 export class Greeting {
+  greetings: string[] = ["Opa", "Hola", "Hello"];
+  users: User[] = [
+    { id: 1, name: "Alex", age: 15 },
+    { id: 2, name: "Bobi", age: 24 },
+    { id: 3, name: "Clark", age: 25 },
+  ];
+  isOldEnough: boolean = true;
+  receivedGreetings: string[] = [];
+  logo: string = "https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg";
+
+  
   protected name = "Koleto";
+
+  onGreet(username: string) {
+    const greeting = `Ko staa we lud - ${username}`
+    this.receivedGreetings.push(greeting)
+  }
 }
